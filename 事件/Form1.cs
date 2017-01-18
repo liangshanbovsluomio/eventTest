dcedef7b79129666207e79fdde47ad748ec3b277
liangshanbovsluomio <114848718@qq.com>
2017-01-19 02:45:25 +0800

    添加项目文件。

---
+++ 事件/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 事件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Size = new Size(150 , 80);
+            btn.Location = new Point(120, 100);
+            btn.Text = "我是被动态创建的";
+
+            btn.Click += Btn_Click;
+            btn.Click += Btn_Click1;
+            btn.Click -= Btn_Click;
+
+            this.Controls.Add(btn);
+        }
+
+        private void Btn_Click1(object sender, EventArgs e)
+        {
+            MessageBox.Show("我也被点击了");
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("我被点击了");
+        }
+    }
+}
---
+++ 自定义事件（播放器）/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 自定义事件_播放器_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayMusic p = new PlayMusic("告白气球");
+            
+            p.DelPlayOver += P_DelPlayOver; //注册事件
+            p.PlaySong();
+        }
+
+        private void P_DelPlayOver(object sender, EventArgs e)
+        {
+            PlayMusic p = sender as PlayMusic; //此处需要调用类的属性Name，因此将sender强转PlayMusic对象
+            MessageBox.Show("《"+p.Name + "》播放完成");
+        }
+
+        class PlayMusic
+        {
+            public event EventHandler DelPlayOver;
+
+            public string Name { get; set; }
+
+            public PlayMusic(string name)
+            {
+                this.Name = name;
+
+            }
+            public void PlaySong()
+            {
+                Console.WriteLine("正在播放{0}"+this.Name);
+                Thread.Sleep(2000);
+
+                
+                if (DelPlayOver != null)
+                {
+                    EventArgs e = new EventArgs();
+                    DelPlayOver(this, e);
+                }
+            }
+        }
+        
+    }
+}
